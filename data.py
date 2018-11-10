de0f2b4f28dd1f990429dd47245adea783163136
unknown <zhou671@purdue.edu>
2018-11-10 00:43:15 -0500

    updated data script

--- data.py
+++ data.py
@@ -6,31 +6,51 @@
 #####################################
 
 import numpy as np
-import random
+#import random
 
 def run():
-	N = 800
-	datap =  np.load('x_clean.npy')
-	count = 1
+	data =  np.load('inputData\\x_clean.npy')
+	data = data[:, 1:8]
+	np.random.shuffle(data)
+	np.random.shuffle(data)
 
-	while (count < 9) :
-		tempData = np.array(random.sample(datap, 100))
+	data_pos = data[np.where(data[:,6] == 1)]
+	data_neg = data[np.where(data[:,6] == -1)]
 
-		if ((tempData==-1).sum() != 8 & (tempData==-1).sum() != 9):
-			tempData = np.array(random.sample(datap, 100))
-			print 'not good'
+	np.random.shuffle(data_pos)
+	np.random.shuffle(data_pos)
+
+	np.random.shuffle(data_neg)
+	np.random.shuffle(data_neg)
+
+	for i in range(7):
+		namex = "data\\X" + str(i) + ".npy"
+		namey = "data\\Y" + str(i) + ".npy"
 		
-		else:
-			print 'good'
-			xfile = "./dataset"+ str(count) + "_X"
-			yfile = "./dataset"+ str(count) + "_Y"
-			np.save(xfile, tempData[:, 1:7])
-			np.save(yfile, tempData[:, 7])
+		x = np.zeros((100, 6))
+		y = np.zeros((100))
 
-			for i in range(100):
-				np.delete(datap, tempData[i][0])
-		
-			count = count + 1
+		x[0:92] = data_pos[i*92:i*92+92, 0:6]
+		y[0:92] = data_pos[i*92:i*92+92, 6]
+		x[92:100] = data_neg[i*8:i*8+8, 0:6]
+		y[92:100] = data_neg[i*8:i*8+8, 6]
+
+		np.save(namex, x)
+		np.save(namey, y)
+
+	namex = "data\\X7.npy"
+	namey = "data\\Y7.npy"
+
+	x = np.zeros((100, 6))
+	y = np.zeros((100))
+
+	x[0:91] = data_pos[644:735, 0:6]
+	y[0:91] = data_pos[644:735, 6]
+	x[91:100] = data_neg[56:65, 0:6]
+	y[91:100] = data_neg[56:65, 6]
+
+	np.save(namex, x)
+	np.save(namey, y)
 
 if __name__ == '__main__':
 	run() 
